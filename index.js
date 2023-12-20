d56b9bd627b62a09e49b755fcb159a3b2e34d001
Eran <eranmaz58@gmail.com>
2023-12-20 22:30:03 +0200

    working

--- index.js
+++ index.js
@@ -1,53 +1,67 @@
-const fs = require('fs');
-const express = require('express')
-const app = express()
-const port = +process.argv[2] || 3000
+const fs = require("fs");
+const Fastify = require("fastify");
+const app = Fastify();
+const port = +process.argv[2] || 3000;
 
-const client = require('redis').createClient()
-client.on('error', (err) => console.log('Redis Client Error', err));
+const Redis = require("ioredis").Redis;
+const client = new Redis({});
+client.on("error", (err) => console.log("Redis Client Error", err));
 
-client.on('ready', () => {
-    app.listen(port, '0.0.0.0', () => {
-        console.log(`Example app listening at http://0.0.0.0:${port}`)
-    })
-})
+app.listen({ port }, () => {
+  console.log(`Example app listening at http://0.0.0.0:${port}`);
+});
 
-const cardsData = fs.readFileSync('./cards.json');
+const cardsData = fs.readFileSync("./cards.json");
 const cards = JSON.parse(cardsData);
 
 async function getMissingCard(key) {
-    const userCards = await client.zRange(key, 0, -1)
-    let allCards = [...cards]
+  const userCards = await client.zrange(key, 0, -1);
+  const cardsGiven = new Set(userCards);
+  const allCards = cards.filter((value) => !cardsGiven.has(value.id));
 
-    userCards.forEach((userCard, idx) => {
-        allCards = allCards.filter(function (value, index, arr) {
-            return JSON.parse(userCard).id !== value.id;
-        })
-    })
-
-    return allCards.pop();
+  return allCards.pop();
 }
 
-app.get('/card_add', async (req, res) => {
-    const  key = 'user_id:' + req.query.id
-    let missingCard = ''
-    while (true){
-        missingCard =await getMissingCard(key);
-        if(missingCard === undefined){
-            res.send({id: "ALL CARDS"})
-            return
-        }
-        result = await client.ZADD(key, {score: 0, value: JSON.stringify(missingCard)}, 'NX')
-        if(result === 0){
-            continue
-        }
-        break
+app.get(
+  "/card_add",
+  {
+    schema: {
+      response: {
+        200: {
+          type: "object",
+          properties: {
+            id: {
+              type: "string",
+            },
+            name: {
+              type: "string",
+            },
+          },
+          required: ["id"],
+        },
+      },
+    },
+  },
+  async (req, res) => {
+    const key = req.query.id;
+    let missingCard = "";
+    while (true) {
+      // console.time("get-missing-card" + key);
+      missingCard = await getMissingCard(key);
+      // console.timeEnd("get-missing-card" + key);
+      if (missingCard === undefined) {
+        res.send({ id: "ALL CARDS" });
+        return;
+      }
+      const result = await client.zadd(key, "NX", [0, missingCard.id]);
+
+      if (!result) continue;
+      break;
     }
-    res.send(missingCard)
-})
+    res.send(missingCard);
+  }
+);
 
-app.get('/ready', async (req, res) => {
-    res.send({ready: true})
-})
-
-client.connect();
+app.get("/ready", async (_, res) => {
+  res.send({ ready: true });
+});
