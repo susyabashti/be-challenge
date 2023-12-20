f0c88a37f10afd50325729b8ff60c557c9a532c3
Eran <eranmaz58@gmail.com>
2023-12-20 23:02:34 +0200

    12.37 seconds top

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
 const fs = require("fs");
 const Fastify = require("fastify");
+const Redis = require("ioredis").Redis;
+const cards = require("./cards.json");
+
 const app = Fastify();
 const port = +process.argv[2] || 3000;
 
-const Redis = require("ioredis").Redis;
-const client = new Redis({});
-client.on("error", (err) => console.log("Redis Client Error", err));
+const client = new Redis();
+app.decorate("redis", client);
 
 app.listen({ port }, () => {
   console.log(`Example app listening at http://0.0.0.0:${port}`);
 });
-
-const cardsData = fs.readFileSync("./cards.json");
-const cards = JSON.parse(cardsData);
-
-async function getMissingCard(key) {
-  const userCards = await client.zrange(key, 0, -1);
-  const cardsGiven = new Set(userCards);
-  const allCards = cards.filter((value) => !cardsGiven.has(value.id));
-
-  return allCards.pop();
-}
 
 app.get(
   "/card_add",
@@ -44,21 +35,8 @@ app.get(
   },
   async (req, res) => {
     const key = req.query.id;
-    let missingCard = "";
-    while (true) {
-      // console.time("get-missing-card" + key);
-      missingCard = await getMissingCard(key);
-      // console.timeEnd("get-missing-card" + key);
-      if (missingCard === undefined) {
-        res.send({ id: "ALL CARDS" });
-        return;
-      }
-      const result = await client.zadd(key, "NX", [0, missingCard.id]);
-
-      if (!result) continue;
-      break;
-    }
-    res.send(missingCard);
+    const currentIndex = await app.redis.incr(key);
+    res.send(cards[currentIndex - 1] || { id: "ALL CARDS" });
   }
 );
 
