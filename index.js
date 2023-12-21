d7e0cb7e5b98eb40b33f71c82b856958ef64ba67
Eran <eranmaz58@gmail.com>
2023-12-21 17:58:34 +0200

    enabled autopipelining

--- index.js
+++ index.js
@@ -2,42 +2,22 @@ const Fastify = require("fastify");
 const Redis = require("ioredis").Redis;
 const cards = require("./cards.json");
 
+const cards_stringified = cards.map((val) => JSON.stringify(val));
+
 const app = Fastify();
 const port = +process.argv[2] || 3000;
 
-const ALL_CARDS = { id: "ALL CARDS" };
-const client = new Redis();
-const schema = {
-  response: {
-    200: {
-      type: "object",
-      properties: {
-        id: {
-          type: "string",
-        },
-        name: {
-          type: "string",
-        },
-      },
-      required: ["id"],
-    },
-  },
-};
+const ALL_CARDS = JSON.stringify({ id: "ALL CARDS" });
+const client = new Redis({ enableAutoPipelining: true });
 
 app.listen({ port }, () => {
   console.log(`Example app listening at http://0.0.0.0:${port}`);
 });
 
-app.get(
-  "/card_add",
-  {
-    schema,
-  },
-  async (req, res) => {
-    const currentIndex = await client.incr(req.query.id);
-    res.send(cards[currentIndex - 1] || ALL_CARDS);
-  }
-);
+app.get("/card_add", async (req, res) => {
+  const currentIndex = await client.incr(req.query.id);
+  res.send(cards_stringified[currentIndex - 1] || ALL_CARDS);
+});
 
 app.get("/ready", async (_, res) => {
   res.send({ ready: true });
