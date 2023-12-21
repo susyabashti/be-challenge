18a2abb17f250ea2a6ec9484a20ee8d583a43f03
Eran <eranmaz58@gmail.com>
2023-12-21 17:43:28 +0200

    test v3

--- index.js
+++ index.js
@@ -1,44 +1,33 @@
-const Fastify = require("fastify");
+const http = require("http");
 const Redis = require("ioredis").Redis;
+const url = require("url");
 const cards = require("./cards.json");
 
-const app = Fastify();
 const port = +process.argv[2] || 3000;
+const ALL_CARDS = JSON.stringify({ id: "ALL CARDS" });
+const READY = JSON.stringify({ ready: true });
+const client = new Redis({ enableAutoPipelining: true });
 
-const ALL_CARDS = { id: "ALL CARDS" };
-const client = new Redis();
-const schema = {
-  response: {
-    200: {
-      type: "object",
-      properties: {
-        id: {
-          type: "string",
-        },
-        name: {
-          type: "string",
-        },
-      },
-      required: ["id"],
-    },
-  },
-};
+const cards_stringified = cards.map((val) => JSON.stringify(val));
 
-app.listen({ port }, () => {
-  console.log(`Example app listening at http://0.0.0.0:${port}`);
+const app = http.createServer();
+
+app.on("request", async (req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+
+  switch (parsedUrl.pathname) {
+    case "/card_add":
+      const currentIndex = await client.incr(parsedUrl.query.id);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(cards_stringified[currentIndex - 1] || ALL_CARDS);
+      break;
+    case "/ready":
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(READY);
+      break;
+  }
 });
 
-app.get(
-  "/card_add",
-  {
-    schema,
-  },
-  async (req, res) => {
-    const currentIndex = await client.incr(req.query.id);
-    res.send(cards[currentIndex - 1] || ALL_CARDS);
-  }
-);
-
-app.get("/ready", async (_, res) => {
-  res.send({ ready: true });
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Example app listening at http://0.0.0.0:${port}`);
 });
